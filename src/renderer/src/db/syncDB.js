131eb4d5221ff75d77b06d3d283b74cd3596c3b6
zzzlew <1400377637@qq.com>
2026-03-08 22:15:08 +0800

    怎么有个文件忘记提交了，这个不知道怎么回事，这个文件自动忽略了

---
+++ src/renderer/src/db/syncDB.js
@@ -0,0 +1,64 @@
+import { updateConversation, uploadNoteContent, updateOldNoteContent } from "./dualDB";
+import { uploadNoteContentApi, updateOldNoteContentApi } from '../api/Favorites'
+import { updateConversationTopStatusApi, updateConversationMuteStatusApi } from '../api/Conversation'
+
+/**
+ * 
+ * @param {*} conversationId -- 会话id
+ * @param {*} userId -- 用户id
+ * @param {*} status -- 更新状态
+ */
+const updateConversationTopStatus = (conversationId, userId, status) => {
+    // 更新本地数据库中的置顶状态
+    const condition = {
+        id: conversationId,
+        userId: userId
+    }
+    const data = {
+        isTop: status
+    }
+    updateConversation(condition, data)
+    // 更新服务端的置顶状态
+    updateConversationTopStatusApi(conversationId, status)
+}
+
+/**
+ * 
+ * @param {*} conversationId -- 会话id
+ * @param {*} userId -- 用户id
+ * @param {*} status -- 更新状态
+ */
+const updateConversationMuteStatus = (conversationId, userId, status) => {
+    // 更新本地数据库中的消息免打扰状态
+    const condition = {
+        id: conversationId,
+        userId: userId
+    }
+    const data = {
+        isMute: status
+    }
+    updateConversation(condition, data)
+    // 更新服务端的消息免打扰状态
+    updateConversationMuteStatusApi(conversationId, status)
+}
+
+const insertNewNote = (data) => {
+    // 保存到本地数据库
+    uploadNoteContent(data)
+    // 保存到服务器数据库
+    uploadNoteContentApi(data)
+}
+
+const updateOldNote = (condition, data) => {
+    // 保存到本地数据库
+    updateOldNoteContent(condition, data)
+    // 保存到服务器数据库
+    updateOldNoteContentApi(condition, data)
+}
+
+export {
+    updateConversationTopStatus,
+    updateConversationMuteStatus,
+    insertNewNote,
+    updateOldNote
+}
