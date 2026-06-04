714f50bd0851b6a84efca08d8e78e789bb881b9e
zzzlew <1400377637@qq.com>
2026-06-05 00:24:53 +0800

    朋友圈的瀑布流界面

--- src/renderer/src/views/moments/Moments.vue
+++ src/renderer/src/views/moments/Moments.vue
@@ -8,7 +8,11 @@
     <!-- 帖子列表 -->
     <div class="moments-content">
       <el-scrollbar ref="scrollbarRef" style="height: 100%; width: 100%">
-        <div class="post-list">
+        <div class="post-list" :class="{ 'is-reflowing': isReflowing }">
+          <div v-if="postList.length === 0" class="empty-state">
+            <p>还没有动态，快来发布第一条吧！</p>
+          </div>
+
           <!-- 单条动态 -->
           <div class="post-item" v-for="post in postList" :key="post.id">
             <!-- 用户信息头部 -->
@@ -37,7 +41,7 @@
               <div class="post-text">{{ post.content }}</div>
               <!-- 图片展示 -->
               <div class="post-images" v-if="post.images && post.images.length > 0">
-                <img v-for="(img, index) in post.images" :key="index" :src="img" class="post-image"
+                <img v-for="(img, index) in post.images" :key="index" :src="img" class="post-image" loading="lazy"
                   @click="previewImage(post.images, index)" />
               </div>
             </div>
@@ -51,7 +55,7 @@
                 </el-icon>
                 <span>{{ post.likeCount > 0 ? post.likeCount : '点赞' }}</span>
               </div>
-              <div class="action-item" @click="handleComment(post.id)">
+              <div class="action-item" :class="{ active: post.showComments }" @click="handleComment(post.id)">
                 <el-icon>
                   <ChatLineRound />
                 </el-icon>
@@ -65,44 +69,46 @@
               </div>
             </div>
 
-            <!-- 评论列表 -->
-            <div class="comment-list" v-if="post.comments && post.comments.length > 0">
-              <div class="comment-item" v-for="comment in post.comments" :key="comment.id">
-                <img :src="comment.avatar" class="comment-avatar" />
-                <div class="comment-content">
-                  <div class="comment-user">{{ comment.username }}</div>
-                  <div class="comment-text">{{ comment.content }}</div>
-                  <div class="comment-footer">
-                    <span class="comment-time">{{ comment.time }}</span>
-                    <div class="comment-actions">
-                      <div class="comment-action" :class="{ active: comment.isLiked }"
-                        @click="handleCommentLike(post.id, comment.id)">
-                        <el-icon>
-                          <StarFilled v-if="comment.isLiked" />
-                          <Star v-else />
-                        </el-icon>
-                        <span v-if="comment.likeCount > 0">{{ comment.likeCount }}</span>
-                      </div>
-                      <div class="comment-action" @click="handleCommentReply(post.id, comment.id, comment.username)">
-                        <el-icon>
-                          <ChatLineRound />
-                        </el-icon>
-                        <span>回复</span>
+            <Transition name="comment-slide">
+              <!-- 评论列表 -->
+              <div class="comment-list" v-if="post.showComments && post.comments && post.comments.length > 0">
+                <div class="comment-item" v-for="comment in post.comments" :key="comment.id">
+                  <img :src="comment.avatar" class="comment-avatar" />
+                  <div class="comment-content">
+                    <div class="comment-user">{{ comment.username }}</div>
+                    <div class="comment-text">{{ comment.content }}</div>
+                    <div class="comment-footer">
+                      <span class="comment-time">{{ comment.time }}</span>
+                      <div class="comment-actions">
+                        <div class="comment-action" :class="{ active: comment.isLiked }"
+                          @click="handleCommentLike(post.id, comment.id)">
+                          <el-icon>
+                            <StarFilled v-if="comment.isLiked" />
+                            <Star v-else />
+                          </el-icon>
+                          <span v-if="comment.likeCount > 0">{{ comment.likeCount }}</span>
+                        </div>
+                        <div class="comment-action" @click="handleCommentReply(post.id, comment.id, comment.username)">
+                          <el-icon>
+                            <ChatLineRound />
+                          </el-icon>
+                          <span>回复</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <!-- 回复列表 -->
-                  <div class="reply-list" v-if="comment.replies && comment.replies.length > 0">
-                    <div class="reply-item" v-for="reply in comment.replies" :key="reply.id">
-                      <span class="reply-user">{{ reply.username }}</span>
-                      <span class="reply-to" v-if="reply.replyTo"> 回复 {{ reply.replyTo }}</span>
-                      <span class="reply-text">: {{ reply.content }}</span>
+                    <!-- 回复列表 -->
+                    <div class="reply-list" v-if="comment.replies && comment.replies.length > 0">
+                      <div class="reply-item" v-for="reply in comment.replies" :key="reply.id">
+                        <span class="reply-user">{{ reply.username }}</span>
+                        <span class="reply-to" v-if="reply.replyTo"> 回复 {{ reply.replyTo }}</span>
+                        <span class="reply-text">: {{ reply.content }}</span>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Transition>
           </div>
         </div>
       </el-scrollbar>
@@ -125,27 +131,33 @@ import { ref } from 'vue'
 
 const scrollbarRef = ref()
 
+const isReflowing = ref(false)
+let resizeTimer: NodeJS.Timeout | null = null
+
+// const postList = ref([])
+
 // 模拟数据
 const postList = ref([
   {
     id: 1,
     username: '张三',
-    avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Felix',
     time: '2小时前',
     content: '今天天气真不错，适合出去走走！分享一些随手拍的照片～',
     images: [
-      'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-      'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg'
+      'https://picsum.photos/id/10/800/600',
+      'https://picsum.photos/id/11/800/600'
     ],
     isLiked: false,
     likeCount: 23,
-    commentCount: 5,
+    commentCount: 2,
     isFollowed: false,
+    showComments: false,
     comments: [
       {
         id: 1,
         username: '李四',
-        avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Aneka',
         content: '照片拍得真好看！',
         time: '1小时前',
         isLiked: false,
@@ -162,7 +174,7 @@ const postList = ref([
       {
         id: 2,
         username: '王五',
-        avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=James',
         content: '在哪里拍的呀？',
         time: '50分钟前',
         isLiked: true,
@@ -174,28 +186,237 @@ const postList = ref([
   {
     id: 2,
     username: '李四',
-    avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
-    time: '5小时前',
-    content: '学习新技术真的很有趣，今天又解决了一个难题～',
-    images: [],
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Aneka',
+    time: '3小时前',
+    content: '最近在研究 Electron + Vue3，感觉这套组合真的很强大！',
+    images: [
+      'https://picsum.photos/id/20/800/600'
+    ],
     isLiked: true,
     likeCount: 15,
     commentCount: 3,
     isFollowed: true,
-    comments: []
+    showComments: false,
+    comments: [
+      {
+        id: 3,
+        username: '张三',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Felix',
+        content: '确实，开发效率很高。',
+        time: '2小时前',
+        isLiked: false,
+        likeCount: 2,
+        replies: []
+      },
+      {
+        id: 4,
+        username: '小明',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Spooky',
+        content: '求教程分享！',
+        time: '1小时前',
+        isLiked: true,
+        likeCount: 5,
+        replies: []
+      },
+      {
+        id: 5,
+        username: '小红',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Fluffy',
+        content: '我也在学，一起交流。',
+        time: '30分钟前',
+        isLiked: false,
+        likeCount: 0,
+        replies: []
+      }
+    ]
   },
   {
     id: 3,
     username: '王五',
-    avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=James',
     time: '5小时前',
-    content: '学习新技术真的很有趣，今天又解决了一个难题～',
+    content: '周末的咖啡时光 ☕️',
+    images: [
+      'https://picsum.photos/id/30/800/600',
+      'https://picsum.photos/id/31/800/600',
+      'https://picsum.photos/id/32/800/600'
+    ],
+    isLiked: false,
+    likeCount: 45,
+    commentCount: 1,
+    isFollowed: false,
+    showComments: false,
+    comments: [
+      {
+        id: 6,
+        username: '小美',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Cuddles',
+        content: '这家店在哪？看着不错。',
+        time: '4小时前',
+        isLiked: true,
+        likeCount: 3,
+        replies: []
+      }
+    ]
+  },
+  {
+    id: 4,
+    username: '赵六',
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Tigger',
+    time: '昨天',
+    content: '新买的机械键盘，手感绝了！',
+    images: [
+      'https://picsum.photos/id/40/800/600'
+    ],
+    isLiked: true,
+    likeCount: 88,
+    commentCount: 2,
+    isFollowed: false,
+    showComments: false,
+    comments: [
+      {
+        id: 7,
+        username: '阿强',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Bear',
+        content: '什么轴的？',
+        time: '20小时前',
+        isLiked: false,
+        likeCount: 1,
+        replies: [
+          {
+            id: 102,
+            username: '赵六',
+            replyTo: '阿强',
+            content: '红轴，打字很舒服。'
+          }
+        ]
+      },
+      {
+        id: 8,
+        username: '老王',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Oliver',
+        content: '入坑预警！',
+        time: '18小时前',
+        isLiked: true,
+        likeCount: 10,
+        replies: []
+      }
+    ]
+  },
+  {
+    id: 8,
+    username: '小明',
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Spooky',
+    time: '1小时前',
+    content: '瀑布流布局测试中... 这是一个比较长的动态内容，用来测试不同高度的卡片在瀑布流中的排列效果。技术改变生活，代码编织世界！我们需要更多的内容来填充这个区域。',
+    images: [],
+    isLiked: false,
+    likeCount: 10,
+    commentCount: 1,
+    isFollowed: false,
+    showComments: false,
+    comments: [
+      {
+        id: 9,
+        username: '张三',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Felix',
+        content: '效果看着很棒！',
+        time: '30分钟前',
+        isLiked: true,
+        likeCount: 2,
+        replies: []
+      }
+    ]
+  },
+  {
+    id: 9,
+    username: '小红',
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Fluffy',
+    time: '30分钟前',
+    content: '今天也要元气满满哦！',
+    images: [
+      'https://picsum.photos/id/50/800/600'
+    ],
+    isLiked: true,
+    likeCount: 99,
+    commentCount: 2,
+    isFollowed: true,
+    showComments: false,
+    comments: [
+      {
+        id: 10,
+        username: '小明',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Spooky',
+        content: '加油加油！',
+        time: '20分钟前',
+        isLiked: false,
+        likeCount: 1,
+        replies: []
+      },
+      {
+        id: 11,
+        username: '小美',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Cuddles',
+        content: '元气少女上线。',
+        time: '10分钟前',
+        isLiked: true,
+        likeCount: 5,
+        replies: []
+      }
+    ]
+  },
+  {
+    id: 10,
+    username: '阿强',
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Bear',
+    time: '10分钟前',
+    content: '分享一组极简风摄影，希望大家喜欢。',
+    images: [
+      'https://picsum.photos/id/60/800/600',
+      'https://picsum.photos/id/61/800/600'
+    ],
+    isLiked: false,
+    likeCount: 15,
+    commentCount: 1,
+    isFollowed: false,
+    showComments: false,
+    comments: [
+      {
+        id: 12,
+        username: '老王',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Oliver',
+        content: '构图很有深度。',
+        time: '5分钟前',
+        isLiked: true,
+        likeCount: 2,
+        replies: []
+      }
+    ]
+  },
+  {
+    id: 11,
+    username: '老王',
+    avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Oliver',
+    time: '刚刚',
+    content: '这就是全屏下动态增加列数的秘密！大家觉得这个瀑布流怎么样？',
     images: [],
     isLiked: true,
-    likeCount: 15,
-    commentCount: 3,
+    likeCount: 66,
+    commentCount: 1,
     isFollowed: true,
-    comments: []
+    showComments: false,
+    comments: [
+      {
+        id: 13,
+        username: '阿强',
+        avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Bear',
+        content: '非常丝滑！',
+        time: '刚刚',
+        isLiked: false,
+        likeCount: 0,
+        replies: []
+      }
+    ]
   }
 ])
 
@@ -213,8 +434,10 @@ const handleLike = (postId: number) => {
 }
 
 const handleComment = (postId: number) => {
-  // TODO: 实现评论功能
-  console.log('评论帖子:', postId)
+  const post = postList.value.find(p => p.id === postId)
+  if (post) {
+    post.showComments = !post.showComments
+  }
 }
 
 const handleReward = (postId: number) => {
@@ -289,7 +512,28 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
 }
 
 .post-list {
-  padding: 15px 0;
+  column-width: 280px;
+  column-gap: 15px;
+  padding: 15px;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+/* 强制在窗口较小时也要有 3 列 */
+@media (max-width: 800px) {
+  .post-list {
+    column-count: 3;
+  }
+}
+
+.empty-state {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 80px 20px;
+  color: rgba(67, 243, 255, 0.5);
+  gap: 16px;
 }
 
 /* 右下角悬浮按钮 */
@@ -324,18 +568,45 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
 }
 
 .post-item {
-  margin: 0 15px 15px;
+  /* 瀑布流卡片 “防撕裂”、“防断裂”、“保完整” */
+  break-inside: avoid-column;
+  width: 100%;
+  margin-bottom: 15px;
   padding: 16px;
   background: rgba(67, 243, 255, 0.05);
   border-radius: 12px;
   border: 1px solid rgba(67, 243, 255, 0.25);
   transition: all 0.3s ease;
+  animation: cardEnter 0.5s ease forwards;
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+/* 给每个卡片加延迟，形成错落效果 */
+.post-item:nth-child(3n+1) {
+  animation-delay: 0.1s;
+}
+
+.post-item:nth-child(3n+2) {
+  animation-delay: 0.2s;
+}
+
+.post-item:nth-child(3n+3) {
+  animation-delay: 0.3s;
+}
+
+@keyframes cardEnter {
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 }
 
 .post-item:hover {
   background: rgba(67, 243, 255, 0.08);
   border-color: rgba(67, 243, 255, 0.4);
-  box-shadow: 0 4px 12px rgba(67, 243, 255, 0.15);
+  box-shadow: 0 6px 20px rgba(67, 243, 255, 0.2);
+  transform: translateY(-2px);
 }
 
 .post-header {
@@ -429,6 +700,14 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
   display: grid;
   gap: 8px;
   grid-template-columns: repeat(3, 1fr);
+  opacity: 0;
+  animation: imageFadeIn 0.4s ease forwards;
+}
+
+@keyframes imageFadeIn {
+  to {
+    opacity: 1;
+  }
 }
 
 .post-images:has(img:only-child) {
@@ -441,7 +720,8 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
 
 .post-image {
   width: 100%;
-  height: 150px;
+  height: auto;
+  max-height: 200px;
   object-fit: cover;
   border-radius: 8px;
   cursor: pointer;
@@ -458,7 +738,9 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
 .post-actions {
   display: flex;
   align-items: center;
-  gap: 20px;
+  justify-content: space-between;
+  /* 按钮之间均匀分布 */
+  gap: 8px;
   padding-top: 12px;
   border-top: 1px solid rgba(67, 243, 255, 0.15);
 }
@@ -466,14 +748,19 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
 .action-item {
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 6px;
-  padding: 6px 12px;
+  padding: 6px 4px;
   border-radius: 6px;
   font-size: 13px;
   color: rgba(67, 243, 255, 0.8);
   cursor: pointer;
   transition: all 0.3s ease;
   background: rgba(67, 243, 255, 0.1);
+  white-space: nowrap;
+  flex: 1;
+  max-width: 90px;
+  height: 32px;
 }
 
 .action-item:hover {
@@ -586,6 +873,27 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
   font-size: 13px;
 }
 
+/* 评论展开/收起动画 */
+.comment-slide-enter-active,
+.comment-slide-leave-active {
+  transition: all 0.35s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  overflow: hidden;
+}
+
+.comment-slide-enter-from,
+.comment-slide-leave-to {
+  max-height: 0;
+  opacity: 0;
+  margin-top: 0;
+  padding-top: 0;
+}
+
+.comment-slide-enter-to,
+.comment-slide-leave-from {
+  max-height: 1000px;
+  opacity: 1;
+}
+
 .reply-list {
   margin-top: 8px;
   padding-left: 12px;
@@ -616,5 +924,13 @@ const handleCommentReply = (postId: number, commentId: number, username: string)
 :deep(.el-scrollbar__thumb) {
   background: rgba(67, 243, 255, 0.4);
   border-radius: 4px;
+}
+
+.action-item:active {
+  transform: scale(0.95);
+}
+
+.follow-btn:active {
+  transform: scale(0.95);
 }
 </style>
